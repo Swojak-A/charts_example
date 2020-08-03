c5f44807e42a2e005809eac9d4d32dc5a609dc7f
swojak-a <swojak.a@gmail.com>
2020-08-03 16:22:05 +0200

    feat: add donation_date to Donation model

--- app/modules/trunc_year/models.py
+++ app/modules/trunc_year/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 from modules.core.db import BaseModel
@@ -19,9 +20,12 @@ class Donation(BaseModel):
         choices=DonationTypes.name_choices(),
         default=DonationTypes.donation.name,
     )
+    donation_date = models.DateField(
+        verbose_name=_("Donation Date"), null=False, blank=False, default=timezone.now
+    )
 
     def __str__(self):
-        return f"{self.get_type_display()}: {self.value} from {self.created_at.isoformat()}"
+        return f"{self.get_type_display()}: {self.value} from {self.donation_date}"
 
     class Meta:
         verbose_name = _("Donation")

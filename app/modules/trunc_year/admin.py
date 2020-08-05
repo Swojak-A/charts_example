4d715081ffefe746714a3364fd21eb4bdf86c665
swojak-a <swojak.a@gmail.com>
2020-08-05 22:31:02 +0200

    feat: add DonationReportAdmin to trunc year admin

--- app/modules/trunc_year/admin.py
+++ app/modules/trunc_year/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Donation
+from .models import Donation, DonationReport
+from .chart import DonationCountChart
 
 
 @admin.register(Donation)
@@ -11,3 +12,21 @@ class DonationAdmin(admin.ModelAdmin):
     )
     readonly_fields = ("id",)
     list_display = ("id", "value")
+
+
+@admin.register(DonationReport)
+class DonationReportAdmin(admin.ModelAdmin):
+
+    change_list_template = "admin/templates/donation_report_change_list.html"
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(request, extra_context=extra_context)
+
+        try:
+            qs = response.context_data["cl"].queryset
+        except (AttributeError, KeyError):
+            return response
+
+        donation_count_chart = DonationCountChart(queryset=qs)
+        response.context_data["donation_count_chart"] = donation_count_chart.to_html()
+        return response

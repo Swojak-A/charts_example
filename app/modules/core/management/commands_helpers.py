9345ee2e59a1d0f5a73e2be0f439b432629f108c
swojak-a <swojak.a@gmail.com>
2020-08-15 13:08:25 +0200

    feat: add create_employer_expenses to populate_db

--- app/modules/core/management/commands_helpers.py
+++ app/modules/core/management/commands_helpers.py
@@ -4,6 +4,11 @@ from factory.faker import faker
 
 from django.utils import timezone
 
+from modules.plotly_go_vs_express.models import Employee, YearRoundEmployerExpenses
+from modules.plotly_go_vs_express.tests.factories import (
+    EmployeeFactory,
+    YearRoundEmployerExpensesFactory,
+)
 from modules.trunc_year.models import Donation
 from modules.trunc_year.tests.factories import DonationFactory
 
@@ -11,8 +16,40 @@ from modules.trunc_year.tests.factories import DonationFactory
 # helpers
 fake = faker.Faker()
 
+
+def draw_probability(chance: int):
+    r = fake.pyint(max_value=99)
+    return r < chance
+
+
 # create functions
-def create_donations(no_of_years) -> None:
+def create_employer_expenses(no_of_years: int) -> None:
+    Employee.objects.all().delete()
+    YearRoundEmployerExpenses.objects.all().delete()
+
+    this_year = timezone.now().year
+    y = this_year - no_of_years
+
+    initial_no_of_employees = fake.pyint(min_value=1, max_value=3)
+    for n in range(initial_no_of_employees):
+        EmployeeFactory()
+
+    while y <= this_year:
+        no_of_new_employees = fake.pyint(min_value=1, max_value=3)
+        for n in range(no_of_new_employees):
+            EmployeeFactory()
+        employees = Employee.objects.filter(active=True)
+        for employee in employees:
+            YearRoundEmployerExpensesFactory(employee=employee)
+            if draw_probability(chance=15):
+                employee.active = False
+                employee.save()
+
+        print(f"Created {employees.count()} YearRoundEmployerExpenses for year: {y}")
+        y = y + 1
+
+
+def create_donations(no_of_years: int) -> None:
     Donation.objects.all().delete()
 
     this_year = timezone.now().year

c3fb47676c97b934c035bc9e17af31e076adf711
swojak-a <swojak.a@gmail.com>
2020-08-16 16:28:00 +0200

    feat: update all Charts prepare_queryset method

--- app/modules/plotly_go_vs_express/charts.py
+++ app/modules/plotly_go_vs_express/charts.py
@@ -22,7 +22,7 @@ class EmployerExpensesGoChart(Chart):
         self.queryset = self.prepare_queryset()
         self.from_dataframe = False
 
-    def prepare_queryset(self):
+    def prepare_queryset(self) -> Optional["QuerySet"]:
         if not self.initial_queryset:
             return None
 
@@ -104,12 +104,8 @@ class EmployerExpensesGoChart(Chart):
             legend=dict(
                 title=dict(text=""), yanchor="top", y=0.95, xanchor="left", x=0.01
             ),
-            xaxis = dict(
-                title_text="Fiscal Year"
-            ),
-            yaxis=dict(
-                title_text="Expenses"
-            )
+            xaxis=dict(title_text="Fiscal Year"),
+            yaxis=dict(title_text="Expenses"),
         )
         return fig
 
@@ -123,14 +119,14 @@ class EmployerExpensesGoChart(Chart):
 class EmployerExpensesExpressChart(Chart):
     def __init__(self, queryset: Optional["QuerySet"]):
         self.initial_queryset = queryset
-        self.queryset = self.prepare_queryset(queryset=queryset)
+        self.queryset = self.prepare_queryset()
         self.from_dataframe = True
 
-    def prepare_queryset(self, queryset) -> Optional["QuerySet"]:
-        if not queryset:
+    def prepare_queryset(self) -> Optional["QuerySet"]:
+        if not self.initial_queryset:
             return None
 
-        queryset = queryset.annotate(
+        queryset = self.initial_queryset.annotate(
             employee_name=Concat(
                 "employee__first_name",
                 V(" "),
@@ -145,7 +141,6 @@ class EmployerExpensesExpressChart(Chart):
 
     @property
     def data(self) -> "DataFrame":
-
         if not self.queryset:
             return pd.DataFrame(None)
 

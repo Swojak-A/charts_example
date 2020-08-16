c1f87f0603a427e4c6c08ba618d84dda484f1d53
swojak-a <swojak.a@gmail.com>
2020-08-16 12:47:13 +0200

    feat: add YearRoundEmployerExpensesReportAdmin and required templates

--- app/modules/plotly_go_vs_express/admin.py
+++ app/modules/plotly_go_vs_express/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Employee, YearRoundEmployerExpenses
+from .charts import EmployerExpensesExpressChart
+from .models import Employee, YearRoundEmployerExpenses, YearRoundEmployerExpensesReport
 
 
 @admin.register(Employee)
@@ -26,3 +27,25 @@ class YearRoundEmployerExpensesAdmin(admin.ModelAdmin):
     readonly_fields = ("id", "created_at", "updated_at")
     list_display = ("id", "employee", "year", "value")
     list_display_links = ("id",)
+
+
+@admin.register(YearRoundEmployerExpensesReport)
+class YearRoundEmployerExpensesReportAdmin(admin.ModelAdmin):
+
+    change_list_template = (
+        "admin/plotly_go_vs_express/employer_expenses_report_change_list.html"
+    )
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(request, extra_context=extra_context)
+
+        try:
+            qs = response.context_data["cl"].queryset
+        except (AttributeError, KeyError):
+            return response
+
+        employer_expenses_express_chart = EmployerExpensesExpressChart(queryset=qs)
+        response.context_data[
+            "employer_expenses_express_chart"
+        ] = employer_expenses_express_chart.to_html()
+        return response
